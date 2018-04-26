e805583ae460df16992aca1d202e1f4c442a5437
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2018-04-26 13:35:01 -0400

    Create the basis for the weather app
    obtain the users current location and call to the api for weather information to display on the page

---
+++ Weather/assets/weather.js
@@ -0,0 +1,43 @@
+// Set up vars
+const weatherIcon = document.getElementById("weather-icon");
+const weatherDesc = document.getElementById("weather-desc");
+const temperature = document.getElementById("temp-num");
+const tempType = document.getElementById("temp-type");
+const loc = document.getElementById("location");
+
+// find current location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            getWeather(position.coords.latitude, position.coords.longitude);
+        });
+    } else {
+        /*loc.innerHTML*/console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+// get weather data based on location
+function getWeather(lat, long) {
+    const weatherAPI = "https://fcc-weather-api.glitch.me/api/current?";
+    var weatherAPICurrLoc = weatherAPI + 'lat=' + lat + '&lon=' + long;
+    fetch(weatherAPICurrLoc, { method: "get" })
+    .then(response => response.json())
+    .then(data => {
+        updateDataToUI(data.name, data.weather, data.main.temp);
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error))
+}
+
+// update the data from API
+function updateDataToUI(location, weather, temp) {
+    loc.innerHTML = location; /*location*/
+    weatherIcon.innerHTML = `<img src="${weather[0].icon}" />`;
+    weatherDesc.innerHTML = weather[0].description;
+    console.log(temp);
+    temperature.innerHTML = `${temp}&deg;C`;
+}
+
+window.onload = function() {
+    getLocation();
+};
